efcc3d75c83e998bc60240959862cca2c6426e7e
nobukatsu <nobukatsu@nobukatsu.jp>
2018-04-10 12:04:06 +0900

    Add fishshell settings

--- setup.sh
+++ setup.sh
@@ -2,3 +2,9 @@
 ln -fs ~/dotfiles/.bash_profile ~/.bash_profile
 ln -fs ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -fs ~/dotfiles/.bash ~/.bash
+
+# fish-shell
+if [ -d ~/.config/fish ]; then
+    ln -fs ~/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
+    ln -fs ~/dotfiles/.config/fish/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
+fi
